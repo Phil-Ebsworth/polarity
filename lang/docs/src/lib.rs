37ad3e948c7a4a2305fb3615e25a4e2be5682e0e
Philip-Daniel Ebsworth <84278185+Phil-Ebsworth@users.noreply.github.com>
2025-02-23 18:39:55 +0100

    structure sidebar

--- lang/docs/src/lib.rs
+++ lang/docs/src/lib.rs
@@ -2,6 +2,8 @@ pub mod doc;
 pub mod generate;
 pub mod generate_docs;
 pub mod util;
+pub mod structure;
 
 pub use doc::write_html;
 pub use util::open;
+pub use structure::generate_html_from_paths;
